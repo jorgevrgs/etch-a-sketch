a7e510d7f6c2ad29c47695a1168ce54a7548e990
Jorge Vargas <jorgevargasla@gmail.com>
2020-05-28 17:12:04 -0500

    include select color mode and grid size

--- script.js
+++ script.js
@@ -1,37 +1,83 @@
-const grid = document.getElementById('grid');
-const min = Math.min(grid.clientHeight, grid.clientWidth);
-const rows = 32;
-const cols = 32;
-const boxSize = Math.floor(min / rows);
+const input = document.getElementById('input');
+const color = document.getElementById('color');
+const black = document.getElementById('black');
+const buttons = document.querySelectorAll('.buttons');
 
+let mode = 'color';
+color.classList.add('active');
 
-function createGrid(x, y) {
+// ░█▀▀▀ ░█─░█ ░█▄─░█ ░█▀▀█ ▀▀█▀▀ ▀█▀ ░█▀▀▀█ ░█▄─░█ ░█▀▀▀█ 
+// ░█▀▀▀ ░█─░█ ░█░█░█ ░█─── ─░█── ░█─ ░█──░█ ░█░█░█ ─▀▀▀▄▄ 
+// ░█─── ─▀▄▄▀ ░█──▀█ ░█▄▄█ ─░█── ▄█▄ ░█▄▄▄█ ░█──▀█ ░█▄▄▄█
+
+function toggleButton() {
+    color.classList.toggle('active');
+    black.classList.toggle('active');
+}
+
+function getMatrixSize() {
+    return input.value > 0 ? parseInt(input.value) : 16;
+}
+
+function createGrid() {
+    const grid = document.getElementById('grid');
+    const squareSize = Math.min(grid.clientHeight, grid.clientWidth) * 0.9;
+    let matrixSize = getMatrixSize();
+
+    const boxSize = Math.floor(squareSize / matrixSize);
+
     let html = '';
-    const size = x * y;
+    const size = matrixSize ** 2;
     for (let i = 0; i < size; i++) {
         html += `<div class="box grid-${i}"></div>`;
     }
 
     grid.innerHTML = html;
-    grid.style.gridTemplateColumns = `repeat(${y}, auto)`;
+    grid.style.width = squareSize;
+    grid.style.height = squareSize;
+    grid.style.gridTemplateColumns = `repeat(${matrixSize}, auto)`;
+
+    // ░█▀▀▀ ░█──░█ ░█▀▀▀ ░█▄─░█ ▀▀█▀▀
+    // ░█▀▀▀ ─░█░█─ ░█▀▀▀ ░█░█░█ ─░█──
+    // ░█▄▄▄ ──▀▄▀─ ░█▄▄▄ ░█──▀█ ─░█──
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px;`;
+        box.addEventListener('mouseover', e => {
+            const box = e.target;
+            box.style.backgroundColor = getColor(mode);
+        });
+    });
 }
 
-function getColor() {
-    const r = Math.round(Math.random() * 255) ;
-    const g = Math.round(Math.random() * 255);
-    const b = Math.round(Math.random() * 255);
-    const o = Math.round(Math.random() * 10) / 10;
-    return `rgb(${r}, ${g}, ${b}, ${o})`;
+function getColor(mode = 'color') {
+    switch (mode) {
+        case 'black':
+            return 'black';
+        default:
+            const r = Math.round(Math.random() * 255) ;
+            const g = Math.round(Math.random() * 255);
+            const b = Math.round(Math.random() * 255);
+            const alpha = Math.round(Math.random() * 10) / 10;
+            return `rgb(${r}, ${g}, ${b}, ${alpha})`;
+    }
 }
 
-createGrid(rows, cols);
 
-const boxes = document.querySelectorAll('.box');
+// ░█▀▀▀ ░█──░█ ░█▀▀▀ ░█▄─░█ ▀▀█▀▀ 　 ░█─── ▀█▀ ░█▀▀▀█ ▀▀█▀▀ ░█▀▀▀ ░█▄─░█ ░█▀▀▀ ░█▀▀█ 
+// ░█▀▀▀ ─░█░█─ ░█▀▀▀ ░█░█░█ ─░█── 　 ░█─── ░█─ ─▀▀▀▄▄ ─░█── ░█▀▀▀ ░█░█░█ ░█▀▀▀ ░█▄▄▀ 
+// ░█▄▄▄ ──▀▄▀─ ░█▄▄▄ ░█──▀█ ─░█── 　 ░█▄▄█ ▄█▄ ░█▄▄▄█ ─░█── ░█▄▄▄ ░█──▀█ ░█▄▄▄ ░█─░█
 
-boxes.forEach(box => {
-    box.style.cssText = `width: ${boxSize}px; height: ${boxSize}px;`;
-    box.addEventListener('mouseover', e => {
-        const box = e.target;
-        box.style.backgroundColor = getColor();
+buttons.forEach(btn => {
+    btn.addEventListener('click', e => {
+        const btn = e.target;
+        toggleButton();
+        mode = btn.dataset.mode;
     });
 });
+
+input.addEventListener('change', () => {
+    createGrid();
+});
+
+createGrid();
